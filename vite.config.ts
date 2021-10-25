358e3487e13353aee590d7784a9b802d6a4690df
clementcreusat <clement.creusat@opendigitaleducation.com>
2021-10-25 11:25:50 +0200

    test react

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: '/src/my-page.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/,
-    }
-  }
+  plugins: [react()]
 })
