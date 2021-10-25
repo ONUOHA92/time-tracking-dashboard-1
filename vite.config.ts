76aebb9e6eae8dec688c29e12a55be5aa1820b0c
clementcreusat <clement.creusat@opendigitaleducation.com>
2021-10-25 12:55:57 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: '/src/my-page.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/,
-    }
-  }
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     lib: {
+//       entry: '/src/my-page.ts',
+//       formats: ['es']
+//     },
+//     rollupOptions: {
+//       external: /^lit/,
+//     }
+//   }
+// })
+
+import { defineConfig } from "vite";
+
+export default defineConfig({});
